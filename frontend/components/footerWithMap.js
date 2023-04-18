5c5887a0fc969d81c21cfa03b9bc82498b3de152
Simons-Maddy <xiaochunfu@126.com>
2023-04-18 16:45:32 +0800

    update footer

--- frontend/components/footerWithMap.js
+++ frontend/components/footerWithMap.js
@@ -1,6 +1,7 @@
 import {MailOutlined} from "@ant-design/icons";
 import React, {useEffect, useRef} from "react";
-import {Col, Row} from "antd";
+import {Col, Row, Space} from "antd";
+import Image from "next/image";
 
 export default function FooterCustom(){
     const mapref = useRef(null)
@@ -15,22 +16,37 @@ export default function FooterCustom(){
     },[])
     return(
         <footer className="modal-footer" style={{width:"100%",minWidth:1440,zIndex:1000}}>
-            <Row justify="center">
+            <Row justify="center" >
                 <Col>
                 </Col>
                 <Col style={{textAlign:"left",margin:10}}>
-                    <p>Copyright &copy; 2021-{String(new Date().getFullYear())} |
-                        <a href="https://future.pku.edu.cn/" target="_blank" rel="noreferrer"> College of Future Technology (CFT), </a>
-                        <a href="https://english.pku.edu.cn" target="_blank" rel="noreferrer"> Peking University</a>
-                    </p>
-                    <p>
-                        All Rights Reserved | E-mail:<span> </span>
-                        <a target="_blank"  href="mailto:xiaochunfu@stu.pku.edu.cn" rel="noreferrer">
-                            <MailOutlined />
-                        </a>
-                    </p>
-                    <p><a href="https://beian.miit.gov.cn/integrated/recordquery#/Integrated/recordQuery" target="_blank" id="beian" rel="noreferrer">
-                        苏ICP备2021011214号-1</a></p>
+                    <Row gutter={[0, 0]} style={{width:400}}>
+                        <Col span={24}>
+                            <Space>
+                                <a href="https://english.pku.edu.cn" target="_blank" rel="noreferrer">
+                                    <img src={"/images/index/logo-pku.png"} width={150} height={40} draggable="false" alt={'logo-pku.png'}/>
+                                </a>
+                                <a href="https://future.pku.edu.cn/en" target="_blank" rel="noreferrer">
+                                    <img src={"/images/index/logo-cft.png"} width={110} height={40} draggable="false" alt={'logo-cft.png'}/>
+                                </a>
+                            </Space>
+                        </Col>
+                        <Col span={24}>
+                            <p>Copyright &copy; 2021-{String(new Date().getFullYear())}.
+                                <a href="https://future.pku.edu.cn/en" target="_blank" rel="noreferrer"> College of Future Technology (CFT), </a>
+                                <a href="https://english.pku.edu.cn" target="_blank" rel="noreferrer"> Peking University. </a>
+                                All Rights Reserved.
+                            </p>
+                        </Col>
+                        <Col span={24}>
+                            E-mail:<span> </span>
+                            <a target="_blank"  href="mailto:xiaochunfu@stu.pku.edu.cn" rel="noreferrer">
+                                <MailOutlined />.
+                            </a><span>  </span>
+                            <a href="https://beian.miit.gov.cn/integrated/recordquery#/Integrated/recordQuery" target="_blank" id="beian" rel="noreferrer">
+                                苏ICP备2021011214号-1</a>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </footer>
